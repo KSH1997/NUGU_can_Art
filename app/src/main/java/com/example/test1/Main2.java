bc4fc17b75b85a4dd953c5189d65d14ded2e4cfa
QwQ-is-not-available <omga2333@gmail.com>
2022-11-17 04:43:55 +0900

    add a second window

---
+++ app/src/main/java/com/example/test1/Main2.java
@@ -0,0 +1,14 @@
+package com.example.test1;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Main2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
